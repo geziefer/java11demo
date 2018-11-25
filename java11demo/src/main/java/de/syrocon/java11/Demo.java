3c5d56b1ff584a7185fb266f68cd609b49bc0d67
Alexander Rühl <alexander.ruehl@syrocon.de>
2018-11-25 21:55:26 +0100

    streamline output, omit validation

--- java11demo/src/main/java/de/syrocon/java11/Demo.java
+++ java11demo/src/main/java/de/syrocon/java11/Demo.java
@@ -10,32 +10,30 @@ import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.stream.Collectors;
 
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
-
 public class Demo {
 
 	public static void main(String[] args) throws Exception {
-		// show JEP 323: Local-Variable Syntax for Lambda Parameters
+		System.out.println("*** JEP 323: Local-Variable Syntax for Lambda Parameters");
 		List<String> list = List.of("A", "b", "CD");
-		list = list.stream().map((@NotNull @Size(min = 2, max = 2) var s) -> s.toLowerCase())
+		list = list.stream().map((/* example for using annotation: @NotNull */ var s) -> s.toLowerCase())
 				.collect(Collectors.toList());
 		list.stream().forEach(System.out::println);
 
-		// show JEP 321: HTTP Client (Standard)
+		System.out.println("\n\n*** JEP 321: HTTP Client (Standard)");
 		HttpClient client = HttpClient.newBuilder().build();
-		HttpRequest request = HttpRequest.newBuilder(new URI("https://syrocon.de/")).GET().build();
+		HttpRequest request = HttpRequest.newBuilder(new URI("http://www.brainjar.com/java/host/test.html")).GET()
+				.build();
 		CompletableFuture<HttpResponse<String>> fr = client.sendAsync(request, BodyHandlers.ofString());
 		while (!fr.isDone()) {
 			System.out.println("Waiting...");
-			Thread.sleep(10);
+			Thread.sleep(100);
 		}
 		HttpResponse<String> response = fr.get();
 		System.out.println("Done: " + response.statusCode());
 		response.headers().map().values().stream().forEach(v -> System.out.println(v));
 		System.out.println(response.body());
 
-		// show JEP 181: Nest-Based Access Control
+		System.out.println("\n*** JEP 181: Nest-Based Access Control");
 		Arrays.asList(Demo.class.getNestMembers()).stream().forEach(System.out::println);
 		System.out.println(Demo.Nest.class.getNestHost());
 	}
