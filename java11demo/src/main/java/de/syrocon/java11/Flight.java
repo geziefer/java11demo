8d1aad5915901d5a13d985ac7786a589c1e0aeb8
Alexander Rühl <alexander.ruehl@syrocon.de>
2018-11-26 10:03:02 +0100

    class for flight recorder test

---
+++ java11demo/src/main/java/de/syrocon/java11/Flight.java
@@ -0,0 +1,48 @@
+/*
+ * Taken partly from: https://github.com/chrishantha/sample-java-programs/releases/tag/v0.0.1
+ * Copyright 2015 M. Isuru Tharanga Chrishantha Perera
+ */
+package de.syrocon.java11;
+
+import java.math.BigDecimal;
+import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class Flight {
+
+	public static void main(String[] args) {
+		System.out.println("Starting Application...");
+		final Timer timer = new Timer();
+		timer.schedule(new TimerTask() {
+			public void run() {
+				System.out.println("Exiting now...");
+				System.exit(0);
+			}
+		}, 60000);
+
+		for (int i = 1; i < 100; i++) {
+			Thread thread = new Thread(new MathWorker());
+			thread.setName(String.format("Thread %d: %s", i, "Math"));
+			thread.start();
+		}
+	}
+}
+
+class MathWorker implements Runnable {
+
+	private Random random = new Random();
+
+	@Override
+	public void run() {
+		BigDecimal bigDecimal = BigDecimal.ZERO;
+		while (true) {
+			double value = Math.atan(Math.sqrt(Math.pow(random.nextInt(10), random.nextDouble())));
+			if (random.nextInt(10) > 5) {
+				bigDecimal = bigDecimal.add(BigDecimal.valueOf(value));
+			} else {
+				bigDecimal = bigDecimal.subtract(BigDecimal.valueOf(value));
+			}
+		}
+	}
+}
