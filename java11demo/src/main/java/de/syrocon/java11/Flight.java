ec6f2a3715f7cd1b6346688243f0de87086f86d3
Alexander Rühl <alexander.ruehl@syrocon.de>
2018-11-26 18:20:53 +0100

    enhance flight recorder demo, add jmx access

--- java11demo/src/main/java/de/syrocon/java11/Flight.java
+++ java11demo/src/main/java/de/syrocon/java11/Flight.java
@@ -11,17 +11,20 @@ import java.util.TimerTask;
 
 public class Flight {
 
-	public static void main(String[] args) {
-		System.out.println("Starting Application...");
+	public static void main(String[] args) throws Exception {
+		System.out.println("Wait before application start...");
+		Thread.sleep(10000);
+
+		System.out.println("Starting thread load...");
 		final Timer timer = new Timer();
 		timer.schedule(new TimerTask() {
 			public void run() {
 				System.out.println("Exiting now...");
 				System.exit(0);
 			}
-		}, 60000);
+		}, 30000);
 
-		for (int i = 1; i < 100; i++) {
+		for (int i = 1; i <= 100; i++) {
 			Thread thread = new Thread(new MathWorker());
 			thread.setName(String.format("Thread %d: %s", i, "Math"));
 			thread.start();
